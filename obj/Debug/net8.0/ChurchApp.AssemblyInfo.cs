838684e9bc7031f5031c98ee102fbf469c7b551c
Rowland Adeoye <Rowland.Adeoye@NMDPRA.GOV.NG>
2026-02-21 20:38:27 +0100

    update worker setup

--- obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a850f5671eae120b038c2745fd77e9e40dc782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7211bdcea5478fb10c3490313f229d33648a7dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
