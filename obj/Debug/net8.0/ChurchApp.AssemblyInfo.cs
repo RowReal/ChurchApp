8aeb63b0efed9a8fdb49eb5fde7f65b43382f963
Rowland Adeoye <Rowland.Adeoye@NMDPRA.GOV.NG>
2026-05-24 22:46:43 +0100

    Update sidebar mobile fix2

--- obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c05d90358aed746d72694a0765f4168c45944a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd4f90f921f3609e02bf54a0e0993d15958f253")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
