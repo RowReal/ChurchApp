d9e9ad77e69ab448a01ee89f0aa7dcd9f922645d
Rowland Adeoye <Rowland.Adeoye@NMDPRA.GOV.NG>
2026-07-05 02:00:16 +0100

    Redesigned Record Keeper Nomination page and Guest Search improvements 3

--- obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChurchApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c15dd1db2b39d5eb6765d213f56692ecbedb1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2723667a44b35e80b814d67a0c7abe84bf38e85a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChurchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
